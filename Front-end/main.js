b02bb2e28000df005722f043dea61707aa96e4bf
shreyakarthik1210 <52420053+shreyakarthik1210@users.noreply.github.com>
2023-09-17 12:57:36 -0500

    Add files via upload

--- Front-end/main.js
+++ Front-end/main.js
@@ -1,6 +1,5 @@
 const patientLoginButton = document.getElementById("patientLogin");
 
 patientLoginButton.addEventListener('click', () => {
-  patientLoginButton.style.display = "flex";
-  console.log("Patient Login Button Clicked");
+  console.log('clicked');
 });
